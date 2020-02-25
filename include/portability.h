c212750f5c9b6c504ce1af99d1f6a8b51c7dde33
COKPOWEHEU <31559186+COKPOWEHEU@users.noreply.github.com>
2020-02-25 14:39:37 +0300

    Add some tips and examples
    1. C++ memory allocation with control the result
    2. export C++ function as C functions without decoration
    3. Pack structures without alignment
    4. Does the header included?
    5. Constructor and destructor functions (gcc specific!)
    6. Save label to variable and then goto it.
    7. Format strings to print fixed-length variables (uint8_t etc)
    8. Add binary resources to program (gcc (ld) specific!)
    9. Information about UTF-8 structure
    10. Linux computer name
    11. workaround msvs bug with printf/printf_s-like functions
    12. example to enable UTF-8 (through wchar_t) in console
    13. usage GLUT as static library in mingw

--- include/portability.h
+++ include/portability.h
@@ -51,6 +51,115 @@
   #error "Unsupported platform"
 #endif
 
+/*Прочие подсказки*/
+#if 0
+g++:
+  //запрещает new бросать exception'ы и упрощает проверку
+  #include <new>
+  a = new (std::nothrow)int[10000000];
+    noexcept(a = new int[10000000]); - хуже
+  //стандартный способ С++ поймать ошибку:
+  try{
+    int *p = new int[10000000000];
+  }catch(std::bad_alloc){
+    printf("Err\n");
+  }
+
+  //обертка вокруг С++ функций чтобы избежать декорирования
+  #ifdef __cplusplus
+  extern "C" {
+  #endif
+  ...
+  #ifdef __cplusplus
+  }
+  #endif
+  
+  //упаковка структур
+  #pragma pack(push, 1)
+  struct __attribute__((__packed__)) StructName{
+  ...
+  };
+  #pragma pack(pop)
+
+  //запрещает игнорировать возвращаемое значение (вроде только с С++17)
+  [[nodiscard]] int func(int)
+  
+  //проверка существования заголовка (вроде только с С++17, но gcc вполне понимает)
+  #if __has_include("myinclude.h")
+  #include "myinclude.h"
+  #endif
+
+  //конструкторы - деструкторы (gcc-специфичное)
+  __attribute__((constructor))
+
+gcc/g++
+  //включает распараллеливание по стандарту openmp
+  -fopenmp
+  //TODO: добавить пример кода
+
+  //сохранение метки в переменную
+  void *p = &&lab2;
+  goto *p;
+  lab2:;
+  
+  //форматные макроконстанты для ввода-вывода переменных фиксированного размера вроде int8_t:
+  Использование: scanf("%" SCNxFAST16, &f16_var);
+  PRI(t)(modif)(size)
+  SCN(t)(modif)(size)
+  t - обычные print-scan-овские модификаторы вроде i, d, x и т.п.
+  modif - ничего, FAST, LEAST
+  size - размер переменной: 8, 16 и т.п.
+  Например, PRIdLEAST32, PRId8
+  Кроме того:
+  PRIdMAX PRIdPTR PRIiMAX PRIiPTR
+  
+  //добавление бинарных ресурсов в программу:
+  $ gcc main.o -Wl,--format=binary -Wl,some.res -Wl,--format=default -o a.out
+  extern const uint8_t my_res_start[]   asm("_binary_some_res_start");
+  extern const uint8_t my_res_end[]     asm("_binary_some_res_end");
+  
+UTF-8:
+  0xxxxxxx
+  110xxxxx 10xxxxxx
+  1110xxxx 10xxxxxx 10xxxxxx
+  11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+  
+Linux computer name
+  /sys/devices/virtual/dmi/id/product_name
+  /sys/devices/virtual/dmi/id/board_name
+  
+Microsoft compiler workaround to use standard functions
+  #define _CRT_SECURE_NO_WARNINGS
+
+  
+UTF-8 text to console:
+  #include <stdio.h>
+  #include <wchar.h>
+  #include <locale.h>
+  #ifdef WIN32
+    #include <windows.h>
+    #include "fcntl.h"
+    __attribute__((constructor)) void coninit(){
+      SetConsoleCP(CP_UTF8); SetConsoleOutputCP(CP_UTF8);
+      _setmode(_fileno(stdout), _O_U8TEXT);
+      _setmode(_fileno(stdin), _O_U8TEXT);
+    }
+  #endif
+ 
+  int main(){
+    setlocale(LC_ALL, "");
+    wprintf(L"x\u00B2+7x+4\n");
+    return 0;
+  }
+
+  
+GLUT:
+  cflags += -DFREEGLUT_STATIC
+  ldflags +=  -lfreeglut_static -static-libgcc -static-libstdc++ -lmingw32 -mconsole -mwindows -lopengl32 -lglu32 -lwinmm
+  #Именно в таком порядке!
+  
+#endif
+
 #if 0 //Linux: ввод символа в неканоничном режиме (без необходимости enter'а)
 #include <termios.h>
 static int getch(){
